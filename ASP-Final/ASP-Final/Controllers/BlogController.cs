1a185ae717f169f49911a7a35bd746a06b25d338
ruslanberz <ruslan.berz@gmail.com>
2019-01-08 12:00:01 +0400

    Blog Ready

--- ASP-Final/ASP-Final/Controllers/BlogController.cs
+++ ASP-Final/ASP-Final/Controllers/BlogController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASP_Final.Models;
+using ASP_Final.DAL;
 
 namespace ASP_Final.Controllers
 {
     public class BlogController : Controller
     {
+
+        AspFinalContext db = new AspFinalContext();
         // GET: Blog
         public ActionResult Index()
         {
-            return View();
+            List<Blog> blogs = db.Blogs.ToList();
+            
+            return View(blogs);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            if (Id != 0)
+            {
+                Blog bg = db.Blogs.Find(Id);
+                return View(bg);
+            }
+
+            return HttpNotFound();
+
+           
         }
     }
 }
